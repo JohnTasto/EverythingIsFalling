d33cea4392b39747d9f58e8d2a12db51157d9658
John Tasto <john@tasto.net>
2016-05-20 16:54:24 -0700

    Convert tabs to spaces in hamburger component

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -14,71 +14,71 @@ function Hamburger(props) {
   const halfHeight = height / 2
   const halfStroke = stroke / 2
 
-	const styles = {
-		container: {
+  const styles = {
+    container: {
       height: `${height}px`,
-			width: `${width}px`,
-			position: 'relative',
+      width: `${width}px`,
+      position: 'relative',
       transform: 'rotate(0deg)',
       transition: `${animationDuration}s ease-in-out`,
       cursor: 'pointer',
-		},
-		line: {
-			display: 'block',
+    },
+    line: {
+      display: 'block',
       position: 'absolute',
-			height: `${stroke}px`,
-			width: '100%',
-			background: color,
+      height: `${stroke}px`,
+      width: '100%',
+      background: color,
       borderRadius: `${props.borderRadius}px`,
       opacity: '1',
       left: '0',
       transition: `${animationDuration}s ease-in-out`,
       transform: 'rotate(0deg)',
-		},
-		lineTop: {
+    },
+    lineTop: {
       top:       !isOpen ?    '0' : `${halfHeight - halfStroke}px`,
       transform: !isOpen ? 'none' : 'rotate(135deg)',
-		},
-		lineMiddle: {
-			opacity:   !isOpen ?    '1' : '0',
-			top:       `${halfHeight - halfStroke}`,
+    },
+    lineMiddle: {
+      opacity:   !isOpen ?    '1' : '0',
+      top:       `${halfHeight - halfStroke}`,
       width:     !isOpen ? '100%' : '0',
       left:      !isOpen ?    '0' : '50%',
       transform: !isOpen ? 'none' : 'rotate(180deg)',
-		},
-		lineBottom: {
+    },
+    lineBottom: {
       top:       !isOpen ? `${height - stroke}px` : `${halfHeight - halfStroke}px`,
       transform: !isOpen ?                 'none' : 'rotate(225deg)',
-		},
-	}
+    },
+  }
 
-	return (
+  return (
     <div style={style}>
-  		<div
+      <div
         style={styles.container}
         onClick={e => {
           e.stopPropagation()
           props.onClick(!isOpen)
         }}
       >
-  		  <span style={[styles.line, styles.lineTop]}></span>
-  		  <span style={[styles.line, styles.lineMiddle]}></span>
-  		  <span style={[styles.line, styles.lineBottom]}></span>
-  		</div>
+        <span style={[styles.line, styles.lineTop]}></span>
+        <span style={[styles.line, styles.lineMiddle]}></span>
+        <span style={[styles.line, styles.lineBottom]}></span>
+      </div>
     </div>
-	)
+  )
 }
 
 Hamburger.PropTypes = {
   onClick: React.PropTypes.func.isRequired,
   style: React.PropTypes.object,
-	isOpen: React.PropTypes.bool,
+  isOpen: React.PropTypes.bool,
   color: React.PropTypes.string,
   height: React.PropTypes.number,
-	width: React.PropTypes.number,
-	stroke: React.PropTypes.number,
-	borderRadius: React.PropTypes.number,
-	animationDuration: React.PropTypes.number,
+  width: React.PropTypes.number,
+  stroke: React.PropTypes.number,
+  borderRadius: React.PropTypes.number,
+  animationDuration: React.PropTypes.number,
 }
 
 export default Radium(Hamburger)
