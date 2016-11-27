8f6dc47e06eb288788d43d050ce8c57ffba64ece
John Tasto <john@tasto.net>
2016-11-27 00:04:51 -0800

    Add favicons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import App from './components/app'
 
 import './scss/index.scss'
 
+require('./img/icons/apple-touch-icon-57x57.png')
+require('./img/icons/apple-touch-icon-114x114.png')
+require('./img/icons/apple-touch-icon-72x72.png')
+require('./img/icons/apple-touch-icon-144x144.png')
+require('./img/icons/apple-touch-icon-120x120.png')
+require('./img/icons/apple-touch-icon-152x152.png')
+require('./img/icons/favicon-32x32.png')
+require('./img/icons/favicon-16x16.png')
+require('./img/icons/mstile-144x144.png')
+
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
